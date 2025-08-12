c2bfea59d786ed1d751e8b2a8594e454217bf9c8
MahanTaps <tapiwat643@gmail.com>
2025-08-12 16:12:33 +0200

    refactor: subclassed MainWindow file to main.py

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from MainWindow import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 
-app = QtWidgets.QApplication(sys.argv)
-MainWindow = QtWidgets.QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(MainWindow)
-MainWindow.show()
-sys.exit(app.exec_())
+class Main(QtWidgets.QMainWindow,Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setupUi(self)
+        
+if __name__=="__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
