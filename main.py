3111f5bbe4e3e726fb5a05ce85381bdffe44ac19
MahanTaps <tapiwat643@gmail.com>
2025-08-12 17:44:28 +0200

    feat: update loadFileDialog to now show the chosen files in the window

--- main.py
+++ main.py
@@ -7,25 +7,33 @@ class Main(QtWidgets.QMainWindow,Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
-    #Slots 
-    def loadFileDialog(self):
-        fname=QFileDialog.getOpenFileName(self,"Select the document","C:/","PDF (*.pdf)")
-        
-    def on_loadMemo_clicked(self):
-        print("btn_LoadMemo clicked!")
-    def on_loadPastPaper_clicked(self):
-        print("btn_loadPastPaper clicked!")
-        self.loadFileDialog()
-        print(self.loadFileDialog())
     #Connections
         self.btn_loadPastPaper.clicked.connect(self.on_loadPastPaper_clicked)
         self.btn_loadMemo.clicked.connect(self.on_loadMemo_clicked)
+    #Slots 
+    def loadFileDialog(self,fileType):
+        fname=QFileDialog.getOpenFileName(self,"Select the document","C:/","PDF Files (*.pdf)")
+        #Output filename to screen 
+        if (fname and fileType=="paper"):
+            self.label_pastPaperFileName.setText(str(fname))
+        elif (fname and fileType=="memo"):
+            self.label_memoFileName.setText(str(fname))
+
+
+    def on_loadMemo_clicked(self):
+        print("btn_LoadMemo clicked!")
+        self.loadFileDialog("memo")
+    def on_loadPastPaper_clicked(self):
+        print("btn_loadPastPaper clicked!")
+        self.loadFileDialog("paper")
 
 
 if __name__=="__main__":
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    window=Main()
+    window.show()
+    # MainWindow = QtWidgets.QMainWindow()
+    # ui = Ui_MainWindow()
+    # ui.setupUi(MainWindow)
+    # MainWindow.show()
     sys.exit(app.exec_())
