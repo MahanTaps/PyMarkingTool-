c7baf3e426e7ded81f000d7256e62d4e9767af06
MahanTaps <tapiwat643@gmail.com>
2025-09-17 16:34:40 +0200

    wip(screenshotter): change the arguments of take_screenshot() to accomodate the data generated by rectmaker

--- screenshotter.py
+++ screenshotter.py
@@ -1,9 +1,9 @@
 
 class ScreenShotter:
     
-    def take_screenshot(page_num,rectangle,doc,q_num,filename):
-        page=doc[page_num]
-        page.set_cropbox(rectangle)
+    def take_screenshot(doc,location,q_num,filename):
+        page=doc[location[1]]
+        page.set_cropbox(location[0])
         pix= page.get_pixmap()
         file_title=filename+"\Question "+str(q_num)+".png"
         pix.save(file_title)
