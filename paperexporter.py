022c49b7e2c2964b785489d41dadf63d2c3e6b47
MahanTaps <tapiwat643@gmail.com>
2026-01-07 14:34:57 +0200

    feat(paperexporter): add basic question extraction in dict form with prep_export

--- paperexporter.py
+++ paperexporter.py
@@ -1,5 +1,6 @@
 from screenshotter import ScreenShotter
 from math12iebmaker import Math12IEBMaker
+from itertools import zip_longest
 import pymupdf
 
 class PaperExporter:
@@ -9,3 +10,24 @@ class PaperExporter:
         self.screenshotter=ScreenShotter(q_doc,a_doc,self.rect_locations)
         self.img_locations=self.screenshotter.batch_filenames
         print(self.img_locations)
+    
+    def make_item(self,val1,val2,val3,val4):
+        keys=['q_num','sect','scored','avail','error','lost','q_location','q_stem','answer']
+        vals=[val1,None,None,None,None,None,val2,val3,val4]
+        return {k:v for (k,v) in zip(keys,vals)}
+
+    def prep_export(self):
+        titles = self.rect_locations['q_titles']
+        q_locations=self.img_locations['q_pics']
+        qstem_locations=self.img_locations['stem_pics']
+        a_locations=self.img_locations['a_pics']
+        paper_data=[]
+        for a,b,c,d in zip_longest(titles,q_locations,qstem_locations,a_locations):
+            paper_data.append(self.make_item(a,b,c,d))
+        return paper_data
+
+            
+    
+
+
+
--- paperexportertest.py
+++ paperexportertest.py
@@ -4,3 +4,4 @@ if __name__=="__main__":
     q_filename="examplepdfs\example.pdf"
     a_filename="examplepdfs\example2.pdf"
     exporter = PaperExporter(q_filename,a_filename)
+    print(exporter.prep_export())
