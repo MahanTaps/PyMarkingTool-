871eb6f9b3d0782b2d90b69091e725d48ed1b2bc
MahanTaps <tapiwat643@gmail.com>
2026-02-09 13:19:56 +0200

    chore(sqlite3modeltest):experiment with connecting a view to the model and accessing the current index whilst it's invisible

--- sqlite3modeltest.py
+++ sqlite3modeltest.py
@@ -1,8 +1,10 @@
 import sqlite3
+import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtSql import *
+from PyQt5.QtWidgets import QApplication,QTableView
 
-
+app = QApplication(sys.argv)
 db= QSqlDatabase.addDatabase("QSQLITE")
 db.setDatabaseName("test.db")
 if(db.open()):
@@ -13,3 +15,8 @@ model= QSqlTableModel(None,db)
 model.setTable("question")
 model.select() #populate table with data 
 print("Record:",model.record(0).value('answer'))
+table_view=QTableView()
+table_view.setModel(model)
+sys.exit(app.exec())
+print('Current Index:',table_view.currentIndex())
+#It seems like there's an infinite loop going on? Maybe I need to set the index first
