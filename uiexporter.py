9b9f5977e0ea611a7b040d86a67fe30faa999cca
MahanTaps <tapiwat643@gmail.com>
2026-09-17 18:39:54 +0200

    refactor(uiexporter): move excel and word exports to standalone functions called by UIExporter

--- uiexporter.py
+++ uiexporter.py
@@ -2,22 +2,30 @@ import pandas as pd
 import os 
 from dotenv import load_dotenv,find_dotenv
 from promptmaker import PromptDataCompiler,PromptXmlRenderer
+from spire.doc import *
+from spire.doc.common import *
 
 from google import genai
 class UIExporter:
     def __init__(self,sql_table):
         self.data=sql_table
         self.excel_writer=ExcelWriter(self.data)
-        self.ai_service=AIFormatter(self.data)
+        self.ai_output=AIFormatter(self.data).export()
+        self.word_writer=WordWriter(self.ai_output)
+        self.export()
 
+    def export(self):
+        self.excel_writer.export()
+        self.word_writer.export()
+
+        
 
 
 class ExcelWriter:
     def __init__(self,sql_table):
         self.data=sql_table
-        self.prep_excel()
-        self.export_excel()
-        os.startfile('GI Marking Sheet v1.2.3.xlsx')
+        self.file_name='GI Marking Sheet v1.2.3.xlsx'
+        
 
     def prep_excel(self):
             xlx_list=[]
@@ -33,20 +41,43 @@ class ExcelWriter:
             export_data=self.prep_excel()
             df2=pd.DataFrame(export_data,columns=['Question number','Section/ Taxonomy Title','Marks Scored','Marks Available','Select Error Type'])
             print("Database Frame: \n",df2)
-            with pd.ExcelWriter('GI Marking Sheet v1.2.3.xlsx',mode='a',if_sheet_exists='overlay') as writer:
+            with pd.ExcelWriter(self.file_name,mode='a',if_sheet_exists='overlay') as writer:
                 df2.to_excel(writer,sheet_name='Input Sheet',startrow=2,startcol=0,index=False,header=False)
 
+    def export(self):
+        self.prep_excel()
+        self.export_excel()
+        os.startfile(self.file_name)
+
 class WordWriter:
-    def __init__(self):
-        pass
-    def write_to_word(self,data):
-         return None
+    def __init__(self,prompt_output):
+        self.filename=r"html_test.docx"
+        self.result=prompt_output
+        
+        
+    def export_to_word(self):
+        document = Document()
+        section = document.AddSection()
+        paragraph=section.AddParagraph()
+        paragraph.AppendHTML(self.result)
+        document.SaveToFile(self.filename,FileFormat.Docx2016)
+        document.Close()
+
+    def open(self):
+        os.startfile(self.filename)
+
+    def export(self):
+        self.export_to_word()
+        self.open()
+
+
+
          
 class AIFormatter:
     def __init__(self,sql_table):
         self.prompt=PromptXmlRenderer('prompt.xml',sql_table).make_prompt()
         self.client=genai.Client(api_key=self.get_api_key())
-        self.run_prompt()
+        
 
     def get_file_text(self,filename):
         with open(filename,"r",encoding="utf-8") as f:
@@ -66,6 +97,11 @@ class AIFormatter:
         print(load_dotenv(env_path))
         API_KEY= os.getenv("MY_API_KEY")
         return API_KEY
+    
+    def export(self):
+        return self.run_prompt()
+
+
 
         
 
